eff36b4ef1421f0347d03665c92ff4323feec5d8
RossieNicolas <43535740+RossieNicolas@users.noreply.github.com>
2019-03-13 11:36:41 +0100

    add react router

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class Main extends Component{
     render(){
         return(
+            <Router>
             <div className="bg-light">
             <p>
                 Welcome to the {this.props.name}
             </p>
                 <p>
-                    Contact the admin <a href="/contact" className="btn btn-outline-primary">Click here</a>
+                    Contact the admin <Link to="/contact" className="btn btn-outline-primary">Click here</a>
                 </p>
                 {!this.props.auth.isAuthenticated() &&
                 <div>
@@ -18,6 +20,7 @@ export default class Main extends Component{
                     <button className="btn btn-outline-secondary" onClick={this.props.auth.login}>Login</button>
                 </div>
                 }
-        </div>)
+                </div>
+            </Router>)
         }
 }
